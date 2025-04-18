b2718f8fa04e6df231d25e3c2cc5474482d77826
cezarylasota <cezary.lasota3@gmail.com>
2025-04-18 10:48:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,27 +2,25 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route("/api/v1.0/predict", methods=["GET"])
+@app.route('/api/v1.0/predict', methods=['GET'])
 def predict():
-    try:
-        # Pobierz wartości z parametrów zapytania
-        a = float(request.args.get("a", 0))
-        b = float(request.args.get("b", 0))
-    except ValueError:
-        # Gdyby podano coś, czego nie da się rzutować na float
-        a, b = 0, 0
-
+    # Pobranie liczb z parametrów zapytania, jeśli brak to domyślnie 0
+    num1 = float(request.args.get('num1', 0))
+    num2 = float(request.args.get('num2', 0))
+    
     # Reguła predykcji
-    result = 1 if (a + b) > 5.8 else 0
-
-    # Zwracany słownik
-    return jsonify({
-        "prediction": result,
-        "features": {
-            "a": a,
-            "b": b
+    prediction = 1 if (num1 + num2) > 5.8 else 0
+    
+    # Stworzenie odpowiedzi
+    response = {
+        'prediction': prediction,
+        'features': {
+            'num1': num1,
+            'num2': num2
         }
-    })
+    }
+    
+    return jsonify(response)
 
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
